2a28f0d672dff1ebdc70a82fee49912b6e927378
tuandat <huynhtuandat05december@gmail.com>
2021-04-26 16:34:03 +0700

    setup client

---
+++ client/src/components/Chat.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+Chat.propTypes = {
+
+};
+
+function Chat(props) {
+    return (
+        <div>
+            <h1>Chat</h1>
+        </div>
+    );
+}
+
+export default Chat;
---
+++ client/src/components/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+Login.propTypes = {
+
+};
+
+function Login(props) {
+    return (
+        <div>
+            <h1>Login</h1>
+
+        </div>
+    );
+}
+
+export default Login;
